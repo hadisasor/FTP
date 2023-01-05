34888d324e48728012e53da1393c788035460fb0
abdulhadi sasor <72065607+hadisasor@users.noreply.github.com>
2023-01-05 15:59:25 +0700

    .

--- FTP/resources/views/Packages/travel/travelpackages.blade.php
+++ FTP/resources/views/Packages/travel/travelpackages.blade.php
@@ -1,7 +1,22 @@
 @include('includes.header')
 
 
+<!-- ======= Hero Section ======= -->
+<section id="hero" class="d-flex align-items-center">
 
+    <div class="container text-center" style="margin-top: 50px;">
+      
+ 
+      <div class="row text-center centering">
+        
+        <div class="col-lg-6 order-1 order-lg-2 hero-img" data-aos="zoom-in" data-aos-delay="200">
+          {{-- <img src="{{asset('img/FTLPmain.png')}}" class="img-fluid animated" alt=""> --}}
+          <!-- <h1 style="font-size: 270%;">Booking <br>Hajj package</h1>  -->
+        </div>
+        </div>
+  </div>
+           
+  </section><!-- End Hero -->
 
 @include('packages.includes.navbar')
 
