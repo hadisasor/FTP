cea9ee22bfb77345a6a5f102adbfc25b30f96e34
abdulhadi sasor <72065607+hadisasor@users.noreply.github.com>
2023-01-06 10:46:47 +0700

    carousal hajjpackages

--- FTP/resources/views/Packages/hajj/hajjpackages.blade.php
+++ FTP/resources/views/Packages/hajj/hajjpackages.blade.php
@@ -9,16 +9,30 @@ $countSaver  = 0;
 <!-- ======= Hero Section ======= -->
   <section id="hero" class="d-flex align-items-center">
 
-    <div class="container text-center" style="margin-top: 50px;">
+    <div class="container text-center" style="margin-top: 100px;">
       
- 
-      <div class="row text-center centering">
-        
-        <div class="col-lg-6 order-1 order-lg-2 hero-img" data-aos="zoom-in" data-aos-delay="200">
-          <img src="{{asset('img/FTLPmain.png')}}" class="img-fluid animated" alt="">
-          <!-- <h1 style="font-size: 270%;">Booking <br>Hajj package</h1>  -->
+      <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <img src="img/FTLPmain.png" class="d-block w-100" alt="...">
+          </div>
+          <div class="carousel-item">
+            <img src="img/FLogo.png" class="d-block w-100" alt="...">
+          </div>
+          <div class="carousel-item">
+            <img src="img/team/team-1.jpg" class="d-block w-100" alt="...">
+          </div>
         </div>
-        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Next</span>
+        </button>
+      </div>
+      
   </div>
            
   </section><!-- End Hero -->
