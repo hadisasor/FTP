28da931cc7827f03747795f460d38564bfa4d8c6
moodxft <mohammad.fatani4@gmail.com>
2023-01-10 11:49:35 +0700

    1

--- FTP/resources/views/Packages/hajj/buypackage.blade.php
+++ FTP/resources/views/Packages/hajj/buypackage.blade.php
@@ -2,8 +2,21 @@
 
 
 
+<section id="hero" class="d-flex align-items-center">
 
+<div class="container text-center" style="margin-top: 100px;">
+  
 
+       
+</section><!-- End Hero -->
+
+<section id="hero" class="d-flex align-items-center">
+
+<div class="container text-center" style="margin-top: 100px;">
+  
+
+       
+</section><!-- End Hero -->
 
 
 
