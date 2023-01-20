ff1d01625056cbea769d6a42714f9ea2979610ce
abdulhadi sasor <72065607+hadisasor@users.noreply.github.com>
2023-01-20 10:18:39 +0700

    .

--- FTP/resources/views/admin/settings/packagemanagement.blade.php
+++ FTP/resources/views/admin/settings/packagemanagement.blade.php
@@ -2,15 +2,30 @@
 
     @include('admin.includes.sidebar')
 
-    
+  <div class="content">
     @include('admin.includes.navbar')
+    <div class=" rounded h-100 p-4">
+      <form action="upload.php" method="post" enctype="multipart/form-data">
+        Select image to upload:
+          <label class="editbtn btn-primary" for="upload">Upload File</label>
+          <input id="upload" type="file">
+      </form>
+      <h3 class="mb-4">Admins Users Table</h3>
+                <table class="table table-hover">
+                    <thead>
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">First Name</th>
+                            <th scope="col">Last Name</th>
+                            <th scope="col">Email</th>
+                            <th scope="col">Edit</th>
+                            <th scope="col">Delete</th>
+                            <th scope="col">check</th>
+                        </tr>
+                    </thead>
+                </table>
+    </div>
+    
 
-    <form action="upload.php" method="post" enctype="multipart/form-data">
-  Select image to upload:
-  <input type="file" name="fileToUpload" id="fileToUpload">
-  <input type="submit" value="Upload Image" name="submit">
-</form>
 
-      
-
-@include('admin.includes.footer')
+@include('admin/includes/footer')
