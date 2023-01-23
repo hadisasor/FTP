d05c1c84e57bb9a5489512078040d417473b48e3
abdulhadi sasor <72065607+hadisasor@users.noreply.github.com>
2023-01-23 17:46:45 +0700

    .

--- FTP/resources/views/admin/settings/packagemanagement.blade.php
+++ FTP/resources/views/admin/settings/packagemanagement.blade.php
@@ -7,18 +7,17 @@
     <div class=" rounded h-100 p-4">
       <form action="upload.php" method="post" enctype="multipart/form-data">
         Select image to upload:
-          <label class="editbtn btn-primary" for="upload">Upload File</label>
+          <label class="editbtn btn-primary" for="upload">Add new Image</label>
           <input id="upload" type="file">
       </form>
-      <h3 class="mb-4">Admins Users Table</h3>
+      <h3 class="mb-4">Image Table</h3>
                 <table class="table table-hover">
                     <thead>
                         <tr>
                             <th scope="col">#</th>
-                            <th scope="col">First Name</th>
-                            <th scope="col">Last Name</th>
-                            <th scope="col">Email</th>
-                            <th scope="col">Edit</th>
+                            <th scope="col">Image</th>
+                            <th scope="col">Image type</th>
+                            <th scope="col">Image location</th>
                             <th scope="col">Delete</th>
                             <th scope="col">check</th>
                         </tr>
