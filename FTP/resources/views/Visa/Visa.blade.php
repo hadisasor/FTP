f417615faa922fd5aaff697c05a822580c057695
abdulhadi sasor <72065607+hadisasor@users.noreply.github.com>
2023-02-06 15:51:32 +0700

    visa form

--- FTP/resources/views/Visa/Visa.blade.php
+++ FTP/resources/views/Visa/Visa.blade.php
@@ -1,46 +1,8 @@
 @include('includes/header')
 
-<section id="hero" class="d-flex align-items-center">
 
 
-<div class="container">
-
-
-  <div class="row">
-    <div class="col-lg-6 d-flex flex-column justify-content-center pt-6 pt-lg-4 order-2 order-lg-1" data-aos="fade-up" data-aos-delay="200">
-      
-      @if(Session::get('login'))
-      <div class="alert alert-danger"> {{Session::get('login')}}</div>
-      @endif
-
-      <h1 style="font-size: 220%;">ห้างหุ้นส่วนจํากัด ฟาตอนี แทรเวิล <br>กิจการฮัจญ์ อัศศิดดีก</h1> 
-      <p>"FATONI...SERAMBI MAKKAH <br>
-          ฟาตอนี...ระเบียงบนครมักกะฮฺ"</p>
-      <p>แพ็คเกจฮัจญ์...กับฟาตอนี แทรเวิล <br>
-        คิดจะทําฮัจญ์...มอบความไว้วางใจไปกับเรา</p>
-      <div class="d-flex justify-content-center justify-content-lg-start">
-       
-        <a href="#about" class="btn-get-started scrollto">Get Started</a>
-      <!--  <a href="https://www.youtube.com/watch?v=jDDaplaOz7Q" class="glightbox btn-watch-video"><i class="bi bi-play-circle"></i><span>Watch Video</span></a>
-      --></div>
-
-
-
-      <!-- change the photo in background -->
-    </div>
-    <div class="col-lg-6 order-1 order-lg-2 hero-img" data-aos="zoom-in" data-aos-delay="200">
-      <img src="{{asset('img/FTLPmain.png')}}" class="img-fluid animated" alt="">
-    </div>
-    <!-- change the photo -->
-
-
-
-  </div>
-</div>
-
-</section><!-- End Hero -->
-
-
+@include('Visa/form')
 
 
 
---
+++ FTP/resources/views/Visa/form.blade.php
@@ -0,0 +1,53 @@
+<section class="reg-sec" style="background-color:#f3f5fa">
+
+
+
+    <div class="row" style="margin-top:7%; margin-left:30%; margin-bottom:2%;">
+    
+    <div class="col-ms-5 px-3 col-md-6">
+    <form class="container-reg register"  action="store" method="post" >
+            <div class="row mb-3 mt-2 col-9 centering">
+              <label for="email" class="form-label text-start">Email:</label>
+              <input type="email" class="form-control" id="email" placeholder="Enter email" name="email" value="" >
+              <span style="color: #e34646;"></span>
+            </div>
+            
+            <div class="row mb-3 centering text-start">
+              <div class="col-9">
+                  <label for="fstname" class="form-label text-start">first name</label>
+                  <input type="text" class="form-control" id="fstname" placeholder="Enter your firstname" name="fstname" value="" style="width:50%;" >
+                  <span style="color: #e34646;"></span>
+              </div>
+              
+              <div class="col-9 mt-2" >
+                  <label for="lstname" class="form-label text-start">last name</label>
+                  <input type="text" class="form-control" id="lstname" placeholder="Enter your lastname" name="lstname" value="" style="width:50%;" >
+                  <span style="color: #e34646;"></span>
+              </div>
+            </div>
+
+            <div class="row mb-3 col-9 centering">
+              <label for="password" class="form-label text-start">Password:</label>
+              <input type="password" class="form-control" id="password" placeholder="Enter password" name="password" pattern="(?=.*\d).{8,}" title="Must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters" >
+              <span style="color: #e34646;"></span>
+
+            </div>
+
+            <div class="row mb-3 col-9 centering">
+            <label for="confirm_password" class="form-label text-start">Confirm password:</label>
+            <input type="password" class="form-control" id="confirm_pwd" placeholder="Confirm password" name="confirm_password">
+            <span style="color: #e34646;"></span>
+            </div>    
+            
+
+            <div class="row mb-3 col-9 centering">
+                <label for="phone" class="form-label text-start">phone number:</label>
+                <input type="number" class="form-control" id="phone" placeholder="enter phone number" name="phone" value="{{old('phone')}}">
+                <span style="color: #e34646;"></span>
+              </div>
+            <br>
+            <button type="submit" class="btn-get-started mb-4">signup</button>
+    </form>  
+    </div>  
+   </div>
+</section>
