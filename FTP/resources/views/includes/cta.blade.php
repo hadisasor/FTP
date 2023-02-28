aa1498d6cb471b76684e600d15db84bfe8da9dd9
abdulhadi sasor <72065607+hadisasor@users.noreply.github.com>
2023-02-28 13:58:47 +0700

    .

--- FTP/resources/views/includes/cta.blade.php
+++ FTP/resources/views/includes/cta.blade.php
@@ -1,5 +1,18 @@
 <!-- ======= Cta Section ======= -->
 <section id="cta" class="cta">
+  <div class="infocon d-flex">
+    <div class="phem col-8 text-start">
+    <p>Phone: +66 99-313-1166</p> 
+    <p>Email:
+    <a href="https://mail.google.com">fatoni.travel@gmail.com</a>
+    </p>
+    </div>
+    <div class="top-social-area col-4">
+      <a href="https://www.facebook.com/fatoni.travel" data-toggle="tooltip" data-placement="bottom" title data-original-title="Facebook"><i class="bx bxl-facebook">
+      </i></a>
+      <a href="#" data-toggle="tooltip" data-placement="bottom" title data-original-title="Line"><i class="bi bi-line"></i></a>
+    </div>
+  </div>
     <div class="container d-flex" data-aos="zoom-in">
 
       <div class="topic row">
@@ -11,18 +24,6 @@
             คิดจะทําฮัจญ์...มอบความไว้วางใจไปกับเรา</p>
         </div>
       </div>
-      <div class="infocon d-flex">
-        <div class="phem col-10 text-start">
-        <p>Phone: +66 99-313-1166</p> 
-        <p>Email:
-        <a href="https://mail.google.com">fatoni.travel@gmail.com</a>
-        </p>
-        </div>
-        <div class="top-social-area col-6">
-          <a href="https://www.facebook.com/fatoni.travel" data-toggle="tooltip" data-placement="bottom" title data-original-title="Facebook"><i class="bx bxl-facebook">
-          </i></a>
-          <a href="#" data-toggle="tooltip" data-placement="bottom" title data-original-title="Line"><i class="bi bi-line"></i></a>
-        </div>
-      </div>
+      
     </div>
   </section><!-- End Cta Section -->
